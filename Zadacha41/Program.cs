3b1d824913fcf3ffa27a48bd739975093c095037
Sergey Zhavoronkov <zhavoronkov-s@mail.ru>
2022-07-14 19:09:49 +0300

    дз к семинару 5, основные задачи

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+*/
+Console.WriteLine("Введите количество символов в массиве:");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+Console.WriteLine("Заполните массив символами, через ENTER");
+
+
+void fillArray(int[] A)
+{
+    for (int i = 0; i < M; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+}
+ 
+void currnetNumber(int[] A)
+{
+    int count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Чисел больше 0 в массиве: {count}");
+}
+
+
+fillArray(array);
+currnetNumber(array);
---
+++ Zadacha43/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями:
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+Console.WriteLine("Программа которая ищет точку пересечения двух прямых, заданных уравнениями: ");
+Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2;");
+
+Console.WriteLine("Введите значение коофициента b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение коофициента k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение коофициента b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение коофициента k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Коофициенты уравнений: {b1} , {k1} , {b2} , {k2}");
+
+// k1*x +b1 = k2*x + b2; k1*x - k2*x = b2 - b1; x(k1 - k1) = b2 - b1; x = (b2-b1)/(k1-k2)
+double b = b2 - b1;
+double k = k1- k2;
+double y = (k1*b2 - k2*b1)/(k1-k2);
+double x = b/k;
+
+// y = k1*x +b1; k1*x = y - b1; x = (y - b1)/k1
+// y = k2((y - b1)/k1) + b2; y = k2 * ((y/k1 - b1/k1)) + b2;  y = (k1*b2 - k2*b1)/(k1-k2)
+// y = k2(x = (y - b1)/k1) + b2
+Console.WriteLine("Координаты точки пересечения: " + x + " ; " + y);
+
