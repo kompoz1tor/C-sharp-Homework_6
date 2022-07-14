79244f0a14eeaa9ce343c03674073c086eb0710f
Sergey Zhavoronkov <zhavoronkov-s@mail.ru>
2022-07-14 20:30:13 +0300

    верное решение

--- Zadacha43/Program.cs
+++ Zadacha43/Program.cs
@@ -18,14 +18,20 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine($"Коофициенты уравнений: {b1} , {k1} , {b2} , {k2}");
 
+if (k1 != k2)
+{
+    double b = b2 - b1;
+    double k = k1- k2;
+    double y = (k1*b2 - k2*b1)/(k1-k2);
+    double x = b/k;
+    Console.WriteLine("Координаты точки пересечения: " + x + " ; " + y);
+}
+else Console.WriteLine("Прямые параллельны");
 // k1*x +b1 = k2*x + b2; k1*x - k2*x = b2 - b1; x(k1 - k1) = b2 - b1; x = (b2-b1)/(k1-k2)
-double b = b2 - b1;
-double k = k1- k2;
-double y = (k1*b2 - k2*b1)/(k1-k2);
-double x = b/k;
+
 
 // y = k1*x +b1; k1*x = y - b1; x = (y - b1)/k1
 // y = k2((y - b1)/k1) + b2; y = k2 * ((y/k1 - b1/k1)) + b2;  y = (k1*b2 - k2*b1)/(k1-k2)
 // y = k2(x = (y - b1)/k1) + b2
-Console.WriteLine("Координаты точки пересечения: " + x + " ; " + y);
+
 
